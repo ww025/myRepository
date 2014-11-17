ea0abc8faf4b7e5a05942df691486a18a47448eb
025ww <784685994@qq.com>
2014-11-17 13:22:40 +0800

    ff

--- DemoGit/src/git/User.java
+++ DemoGit/src/git/User.java
@@ -20,19 +20,25 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public static void main(String[] args) {
-		User user = new User();
-		user.setUsername("ww");
-		user.setPassword("i,ou");
-		User user2 = new User();
-		user2.setUsername("ww1");
-		user2.setPassword("i,ou");
-		System.out.println(user.toString());
-		System.out.println(user2.toString());
-	}
-
 	@Override
 	public String toString() {
 		return "User [username=" + username + ", password=" + password + "]";
 	}
+	
+	public static void main(String[] args) {
+		User user1 = new User();
+		user1.setUsername("ww1");
+		user1.setPassword("i,ou1");
+		User user2 = new User();
+		user2.setUsername("ww2");
+		user2.setPassword("i,ou2");
+		User user3 = new User();
+		user3.setUsername("ww3");
+		user3.setPassword("i,ou3");
+		//System.out.println(user1.toString());
+		System.out.println(user2.toString());
+		System.out.println(user3.toString());
+	}
+
+
 }
